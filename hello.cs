81ff42ca8c7d650f400fc1701c034a1a30b3c116
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-03-20 18:08:18 -0700

    Add files via upload

---
+++ hello.cs
@@ -0,0 +1,10 @@
+namespace Program 
+{ 
+    public class Runner 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
---
+++ hello.sql
@@ -0,0 +1,3 @@
+CREATE TABLE hello (phrase TEXT);
+INSERT INTO hello VALUES ("Hello, world!");
+SELECT COUNT(*) FROM hello;
